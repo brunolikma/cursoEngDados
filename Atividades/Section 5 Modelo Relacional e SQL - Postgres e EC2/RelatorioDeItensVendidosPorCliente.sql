cd9936d34f41fe218d5b2c015f44c6a8719ca0f0
Bruno Lima <72528527+brunolikma@users.noreply.github.com>
2023-01-18 11:07:15 -0300

    Delete Atividades/Section 5 Modelo Relacional e SQL - Postgres e EC2 directory

--- Atividades/Section 5 Modelo Relacional e SQL - Postgres e EC2/RelatorioDeItensVendidosPorCliente.sql
+++
@@ -1,5 +0,0 @@
-select cliente, produto, quantidade, total, data from clientes
-inner join vendas on (clientes.idcliente = vendas.idcliente)
-inner join itensvenda on (vendas.idvenda = itensvenda.idvenda)
-inner join produtos on (itensvenda.idproduto = produtos.idproduto)
-where clientes.idcliente = '8'
